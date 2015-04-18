45bffb9d92e6329e7ca886b00e05472cd4ca102f
bchastain <bjc06200@utdallas.edu>
2015-04-18 00:09:01 -0500

    working on tool code

--- arcspatialfiltering.py
+++ arcspatialfiltering.py
@@ -16,13 +16,32 @@ import time
 ##symmetric = arcpy.GetParameterAsText(10)
 ##alpha = arcpy.GetParameterAsText(11)
 ##alternative = arcpy.GetParameterAsText(12)
+data = "C:\\test.gdb\\SEA"
+descDB = arcpy.Describe(data)
+descNB = arcpy.Describe("C:\\SEA.gal")
 
+if arcpy.env.scratchWorkspace == None:
+    #tempdir = descNB.path
+    tempdir = "C:\\tttt"
+else:
+    tempdir = arcpy.env.scratchWorkspace
 
+maketempdbf = False
+if descDB.dataType == "ShapeFile":
+    data = data.replace(".shp",".dbf")
+elif descDB.dataType == "FeatureClass":
+    maketempdbf = True
+    arcpy.TableToDBASE_conversion([data], tempdir)
+    data = tempdir + "\\" + descDB.basename + ".dbf"
 
 
 start_time = time.time()
-##try:
-##	spatialfiltering.SpatialFiltering(depvar, indepvars, spatiallag, data, nb, style, zeropolicy, tol, zerovalue, ExactEV, symmetric, alpha, alternative)
-##except Exception as e:
-##	arcpy.AddError(e.message)
+try:
+	spatialfiltering.SpatialFiltering(depvar, indepvars, spatiallag, data, nb, style, zeropolicy, tol, zerovalue, ExactEV, symmetric, alpha, alternative)
+except Exception as e:
+	arcpy.AddError(e.message)
+	print e.message
 arcpy.AddMessage("--- %s seconds ---" % (time.time() - start_time))
+print("--- %s seconds ---" % (time.time() - start_time))
+if maketempdbf:
+    arcpy.Delete_management(data)
